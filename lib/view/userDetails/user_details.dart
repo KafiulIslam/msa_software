7f9d15468ca6b7223f59607abeee0680f9015d80
KafiulIslam <kafiulraja135@gmail.com>
2022-12-22 02:32:58 +0600

    email sender feature is implemented

--- lib/view/userDetails/user_details.dart
+++ lib/view/userDetails/user_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:msa_software/controller/constant/constant_widget.dart';
 import '../../controller/api/user_info_api.dart';
@@ -36,8 +37,7 @@ class _UserDetailsState extends State<UserDetails> {
     });
   }
 
-  _phoneCall(String phone) async{
-   //  String numbool?= phone; //set the number here
+  _phoneCall(String phone) async {
     bool? res = await FlutterPhoneDirectCaller.callNumber(phone);
   }
 
@@ -66,12 +66,21 @@ class _UserDetailsState extends State<UserDetails> {
               child: Column(
                 children: [
                   _profile(name, '$street, $city'),
-                  SizedBox(height: screenHeight / 16,),
-                  _infoTile(Icons.email, 'Email',email, (){
-                    print('email');}),
-                  _infoTile(Icons.phone, 'Phone',phone, (){_phoneCall(phone);}),
-                  _infoTile(Icons.web, 'Website',web,null),
-                  _infoTile(Icons.business, 'Company Name',company,null),
+                  SizedBox(
+                    height: screenHeight / 16,
+                  ),
+                  _infoTile(Icons.email, 'Email', email, () async {
+                    await FlutterEmailSender.send(Email(
+                      body: 'Hi $name,\nHappy New Year',
+                      subject: 'Greetings!',
+                      recipients: [email],
+                    ));
+                  }),
+                  _infoTile(Icons.phone, 'Phone', phone, () {
+                    _phoneCall(phone);
+                  }),
+                  _infoTile(Icons.web, 'Website', web, null),
+                  _infoTile(Icons.business, 'Company Name', company, null),
                 ],
               ),
             ),
@@ -105,16 +114,17 @@ class _UserDetailsState extends State<UserDetails> {
     );
   }
 
- Widget _infoTile(IconData icon, String title, String info, VoidCallback? onTap ) {
+  Widget _infoTile(
+      IconData icon, String title, String info, VoidCallback? onTap) {
     return GestureDetector(
       onTap: onTap,
       child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
         margin: const EdgeInsets.symmetric(vertical: 8.0),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 12.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -130,15 +140,23 @@ class _UserDetailsState extends State<UserDetails> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(title,style: fourteenDeepAssStyle,),
-               const SizedBox(height: 3,),
-                Text(info,style: sixteenBlackStyle,),
-              ],),
+                  Text(
+                    title,
+                    style: fourteenDeepAssStyle,
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    info,
+                    style: sixteenBlackStyle,
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
- }
-
+  }
 }
